13c598f1afae0d7e458946430983f141aff3105d
pvictorf <41065334+pvictorf@users.noreply.github.com>
2022-04-09 18:06:02 -0300

    Condition to render Login or Home

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,7 @@ import { LoginScreen } from '../screens/LoginScreen';
 import { HomeScreen } from '../screens/HomeScreen';
 import { MapScreen } from '../screens/MapScreen';
 import { EatsScreen } from '../screens/EatsScreen';
+import { useUserStore } from '../stores/UserStore';
 
 
 export type RootStackParamList  = {
@@ -15,19 +16,28 @@ export type RootStackParamList  = {
   EatsScreen: undefined;
   NavigateCardScreen: undefined;
   RideOptionsCardScreen: undefined;
+  Loginscreen: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList >();
 
 export function Routes() {
+  const user = useUserStore(state => state.user)
+
   return (
     <NavigationContainer>
     <Stack.Navigator>
-      <>
-        <Stack.Screen name='HomeScreen' component={HomeScreen} options={ScreenOptions} />
-        <Stack.Screen name='MapScreen' component={MapScreen} options={ScreenOptions} />
-        <Stack.Screen name='EatsScreen' component={EatsScreen} options={ScreenOptions} />
-      </>
+      {user.name ?  (
+        <>
+          <Stack.Screen name='HomeScreen' component={HomeScreen} options={ScreenOptions} />
+          <Stack.Screen name='MapScreen' component={MapScreen} options={ScreenOptions} />
+          <Stack.Screen name='EatsScreen' component={EatsScreen} options={ScreenOptions} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name='Loginscreen' component={LoginScreen} options={ScreenOptions} />
+        </>
+      )}
     </Stack.Navigator>
   </NavigationContainer>
   );
