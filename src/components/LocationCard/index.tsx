4fd0e1b154d92c156c78f94c741667b0cf981892
pvictorf <41065334+pvictorf@users.noreply.github.com>
2022-04-12 10:05:53 -0300

    Add condition to display LocationCard

--- src/components/LocationCard/index.tsx
+++ src/components/LocationCard/index.tsx
@@ -11,17 +11,19 @@ export interface LocationCardProps {
 }
 
 export const LocationCard = ({placeName, travel}: LocationCardProps) => {
+  if(!travel.totalSeconds) return null;
 
   return (
     <View style={tw`flex-row shadow-sm mt-7 z-10 `}>
       <View style={tw`bg-black px-2 py-1 items-center`}>
-        <Text style={{color: '#fff', fontSize: 16}}>{Number(travel.hours) >= 1 ? travel.hours : travel.minutes}</Text>
+        <Text style={{color: '#fff', fontSize: 16}}>{Number(travel.hours) >= 1 ? `${travel.hours}` : `${travel.minutes}`}</Text>
         <Text style={{color: '#ddd', fontSize: 10}}>{Number(travel.hours) >= 1 ? 'HRS' : 'MIN'}</Text>
       </View>
-      <View style={tw`bg-white px-2 py-1 items-center justify-center`}>
+      <View style={tw`flex-row bg-white p-1 items-center justify-center`}>
         <Text style={tw`text-black text-sm`}>
-          {ellipsisText(placeName, 30)} <IconIonic name='ios-chevron-forward' size={14} color="gray" />
+          {ellipsisText(placeName, 30)} 
         </Text>
+        <IconIonic name='ios-chevron-forward' size={14} color="gray" />
       </View>
     </View>
   )
